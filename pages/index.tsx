7f2de8c45fd9c2596f703bce5b99956d521098de
riccardofano <rcr.fano@gmail.com>
2020-06-29 13:17:34 +0200

    add tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1 className="text-6cl font-semibold italic">Test</h1>
+    </div>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: ["./pages/**/*.ts", "./src/components/**/*.ts"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
