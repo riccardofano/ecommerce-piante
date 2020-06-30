7ec1897d9674af95a1996f996d6298eae99ab11b
riccardofano <rcr.fano@gmail.com>
2020-06-30 15:09:06 +0200

    make gap the same as categories

--- components/productList.tsx
+++ components/productList.tsx
@@ -6,7 +6,7 @@ export default function productList() {
       <h1 className="font-bold text-center text-xl mt-10 mb-4">
         Nuovi prodotti
       </h1>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-2 row-gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-3 row-gap-4">
         <Card />
         <Card />
         <Card />
