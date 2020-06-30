bf2b0d71b3bdae1fe52ae0b11523eaa96bf2c5b9
riccardofano <rcr.fano@gmail.com>
2020-06-30 15:12:01 +0200

    add large size for hero image

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,15 @@ module.exports = {
     screens: {
       sm: "640px",
       md: "768px",
+      lg: "1024px",
     },
     fontFamily: {
       sans: ["Quicksand", "Arial", "sans-serif"],
     },
     extend: {
       width: {
-        768: "calc(768px - 5rem)",
+        "768": "calc(768px - 5rem)",
+        "1024": "calc(1024px - 5rem)",
       },
       colors: {
         white: "#ffffff",
