9ee7d56e36595a1965764bb95b5cb89e15be3833
riccardofano <rcr.fano@gmail.com>
2020-07-01 13:17:11 +0200

    add product page

---
+++ pages/product.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/layout";
+
+export default function product() {
+  return (
+    <>
+      <Layout>
+        <div className="container"></div>
+      </Layout>
+    </>
+  );
+}
