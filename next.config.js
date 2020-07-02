7ba36d7ad22d1bd239c65f222a0fe294927a388e
riccardofano <rcr.fano@gmail.com>
2020-07-02 17:12:50 +0200

    add support for .ico files and remove unused image

--- next.config.js
+++ next.config.js
@@ -2,4 +2,5 @@ const withOptimizedImages = require("next-optimized-images");
 
 module.exports = withOptimizedImages({
   optimizeImagesInDev: true,
+  handleImages: ["jpeg", "png", "svg", "webp", "gif", "ico"],
 });
