5f9c8ce5076d37c3cc70eae3032be758be565bf3
riccardofano <rcr.fano@gmail.com>
2020-07-03 18:36:54 +0200

    check if cart is empty

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,25 @@
 import { InlineIcon } from "@iconify/react";
 import searchLine from "@iconify/icons-clarity/search-line";
 import shoppingCartLine from "@iconify/icons-clarity/shopping-cart-line";
+import shoppingCartSolid from "@iconify/icons-clarity/shopping-cart-solid-badged";
+import { useShoppingCart } from "use-shopping-cart";
+import { useState, useEffect } from "react";
 
 export default function Navbar() {
+  const { cartCount } = useShoppingCart();
+  const [cartEmpty, setCartEmpty] = useState(true);
+  useEffect(() => setCartEmpty(!cartCount), [cartCount]);
+
   return (
     <header className="flex justify-between items-center py-4 container text-lg md:text-2xl">
       <h1 className="text-lg md:text-xl">Piante</h1>
       <div className="flex">
         <InlineIcon className="mr-2 md:mr-6" icon={searchLine} />
-        <InlineIcon icon={shoppingCartLine} />
+        {cartEmpty ? (
+          <InlineIcon icon={shoppingCartLine} />
+        ) : (
+          <InlineIcon icon={shoppingCartSolid} />
+        )}
       </div>
     </header>
   );
