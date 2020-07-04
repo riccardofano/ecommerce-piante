5d1f97dbe99a4b712f073ac56fac0f4ab09ca15d
riccardofano <rcr.fano@gmail.com>
2020-07-04 11:09:53 +0200

    add shopping cart page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,15 @@
+import { useShoppingCart } from "use-shopping-cart";
+import { useState, useEffect } from "react";
+
 import { InlineIcon } from "@iconify/react";
 import searchLine from "@iconify/icons-clarity/search-line";
 import shoppingCartLine from "@iconify/icons-clarity/shopping-cart-line";
 import shoppingCartSolid from "@iconify/icons-clarity/shopping-cart-solid-badged";
-import { useShoppingCart } from "use-shopping-cart";
-import { useState, useEffect } from "react";
 
 export default function Navbar() {
   const { cartCount } = useShoppingCart();
   const [cartEmpty, setCartEmpty] = useState(true);
+
   useEffect(() => setCartEmpty(!cartCount), [cartCount]);
 
   return (
---
+++ pages/cart.tsx
@@ -0,0 +1,50 @@
+import { useShoppingCart } from "use-shopping-cart";
+
+export default function cart() {
+  const {
+    removeItem,
+    cartDetails,
+    decrementItem,
+    incrementItem,
+  } = useShoppingCart();
+
+  const cart = [];
+  for (const sku in cartDetails) {
+    const cartEntry = cartDetails[sku];
+
+    cart.push(
+      <article key={sku}>
+        <img
+          width={200}
+          src={cartEntry.image?.src}
+          alt={`Immagine di ${cartEntry.name}`}
+        />
+        <h1>{cartEntry.name}</h1>
+        <p>Line total: {cartEntry.formattedValue}</p>
+
+        <button
+          onClick={() => decrementItem(cartEntry.sku)}
+          aria-label={`Remove one ${cartEntry.name} from your cart`}
+        >
+          -
+        </button>
+        <p>Quantity: {cartEntry.quantity}</p>
+        <button
+          onClick={() => incrementItem(cartEntry.sku)}
+          aria-label={`Add one ${cartEntry.name} to your cart`}
+        >
+          +
+        </button>
+
+        <button
+          onClick={() => removeItem(cartEntry.sku)}
+          aria-label={`Remove all ${cartEntry.name} from your cart`}
+        >
+          Remove
+        </button>
+      </article>
+    );
+  }
+
+  return cart;
+}
