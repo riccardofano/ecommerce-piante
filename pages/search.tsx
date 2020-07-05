155b5358b2aa5183cd932fb6c100b23c27a04d68
riccardofano <rcr.fano@gmail.com>
2020-07-05 13:58:12 +0200

    add formik for type selection

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,21 +1,31 @@
 import { Type, getTypes } from "../utils/search-options-helpers";
 import { GetServerSideProps } from "next";
+import { Field, Formik } from "formik";
+import { useRouter } from "next/router";
 
 interface SearchProps {
   types: Type[];
 }
 
 export default function Search({ types }: SearchProps) {
+  const { query } = useRouter();
+  const initialValues = {
+    type: query.type || "all",
+  };
+
   return (
-    <div>
-      <select name="types" id="types">
-        {types.map((type, i) => (
-          <option value={type.type} key={i}>
-            {type.type}
-          </option>
-        ))}
-      </select>
-    </div>
+    <Formik initialValues={initialValues} onSubmit={() => {}}>
+      {({ values }) => (
+        <Field as="select" name="type">
+          <option value="all">Tipologia</option>
+          {types.map((type, i) => (
+            <option value={type.type} key={i}>
+              {type.type}
+            </option>
+          ))}
+        </Field>
+      )}
+    </Formik>
   );
 }
 
