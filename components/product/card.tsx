22e32d205f1f4b5ef87bc796f89d3bf6f4f078d1
riccardofano <rcr.fano@gmail.com>
2020-07-07 13:20:47 +0200

    add links to card and suppress hydration warning

--- components/product/card.tsx
+++ components/product/card.tsx
@@ -1,21 +1,27 @@
 import { Product } from "../../model/product";
 import { formatCurrencyString } from "use-shopping-cart";
-import { useEffect, useState } from "react";
+import Link from "next/link";
 
-export default function card({ name, price, imageUrl, currency }: Product) {
-  const [formattedPrice, setFormattedPrice] = useState("");
-  useEffect(() => {
-    setFormattedPrice(formatCurrencyString({ value: price, currency }));
-  }, []);
+export default function card({
+  name,
+  price,
+  imageUrl,
+  currency,
+  sku,
+}: Product) {
   return (
-    <div className="md:text-lg">
-      <img
-        src={require(`../../public${imageUrl}?resize&size=200`)}
-        alt={`Immagine di ${name}`}
-        className="rounded object-cover object-center min-w-full"
-      />
-      <h1 className="font-bold mt-2">{formattedPrice}</h1>
-      <p>{name}</p>
-    </div>
+    <Link href={`/product/${sku}`}>
+      <a className="md:text-lg hover:underline">
+        <img
+          src={require(`../../public${imageUrl}?resize&size=200`)}
+          alt={`Immagine di ${name}`}
+          className="rounded object-cover object-center min-w-full"
+        />
+        <h1 className="font-bold mt-2" suppressHydrationWarning>
+          {formatCurrencyString({ value: price, currency })}
+        </h1>
+        <p>{name}</p>
+      </a>
+    </Link>
   );
 }
