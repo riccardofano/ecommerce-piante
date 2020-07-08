7c7b3f924eacbd354cf3dd1f483b4c75ad5e760f
riccardofano <rcr.fano@gmail.com>
2020-07-08 19:06:40 +0200

    add about page

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,44 +1,55 @@
 import Link from "next/link";
 
 export default function footer() {
+  const help = [
+    {
+      title: "Metodi di pagamento",
+      tag: "payment",
+    },
+    {
+      title: "Tempi di spedizione",
+      tag: "delivery",
+    },
+    {
+      title: "Dove si trova il mio ordine",
+      tag: "order",
+    },
+    {
+      title: "Come effettuare un reso",
+      tag: "return",
+    },
+  ];
   return (
     <section className="mt-20 text-center py-8 bg-gray-lighter">
       <div className="container flex flex-col row-gap-4 md:flex-row md:justify-around items-center text-sm md:text-base footer">
         <div>
-          <h1 className="font-bold uppercase">Aiuto</h1>
+          <Link href="/about">
+            <a>
+              <h1 className="font-bold uppercase hover:underline">Aiuto</h1>
+            </a>
+          </Link>
           <ul>
-            <li>
-              <Link href="#">
-                <a>Metodi di pagamento</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <a>Tempi di spedizione</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <a>Dove si trova il mio ordine</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <a>Come effettuare un reso</a>
-              </Link>
-            </li>
+            {help.map((v, i) => (
+              <li key={i}>
+                <Link href={`/about#${v.tag}`} key={i}>
+                  <a>{v.title}</a>
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
         <div>
-          <h1 className="font-bold uppercase">Contatti</h1>
+          <Link href="/about#contacts">
+            <a>
+              <h1 className="font-bold uppercase hover:underline">Contatti</h1>
+            </a>
+          </Link>
           <ul>
             <li>Telefono: +39 1234 567 890</li>
             <li>Indirizzo: Via Roma, 1</li>
             <li>20121 Milano MI, Italia</li>
             <li>
-              <Link href="#">
-                <a>email: piante@example.it</a>
-              </Link>
+              <a href="mailto:piante@example.it">email: piante@example.it</a>
             </li>
           </ul>
         </div>
